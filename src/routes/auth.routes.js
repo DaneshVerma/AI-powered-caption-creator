40f363b24ab64c0a050910376563471ce3c6e053
Danesh <techydanesh@gmail.com>
2025-08-23 15:16:22 +0530

    login register controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,35 +1,9 @@
 const express = require("express");
-const userModel = require("../models/user.model");
-const jwt = require("jsonwebtoken");
+const { registerController, loginController } = require("../controllers/auth.Controller");
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  const { username, password } = req.body;
-  const existingUser = await userModel.findOne({ username: username });
+router.post("/register", registerController);
 
-  if (existingUser)
-    return res.status(409).json({
-      message: "username already exist",
-    });
-
-  try {
-    const newUser = await userModel.create({
-      username,
-      password,
-    });
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
-    res.cookie("token", token);
-    return res.json({
-      message: "user created succesfully",
-      newUser,
-    });
-  } catch (error) {
-    return res.json({
-      error: error.message,
-    });
-  }
-});
-
-router.post("/login", async (req, res) => {});
+router.post("/login", loginController);
 
 module.exports = router;
