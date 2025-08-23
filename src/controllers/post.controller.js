3d8a3ccea5f559d2bcc78a19794d1bca7d29fe44
Danesh <techydanesh@gmail.com>
2025-08-24 01:31:32 +0530

    imagekit cloude service implemented

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,16 +1,17 @@
 const postModel = require("../models/post.model");
 const generateCaption = require("../services/ai.service");
+const uploadFile = require("../services/storage.service");
 
 async function createPostController(req, res) {
   const file = req.file;
   console.log(file);
   const b64image = new Buffer.from(file.buffer).toString("base64");
   const caption = await generateCaption(b64image);
-  res
-    .json({
-      caption:caption
-    })
-    .status(300);
+  const result = await uploadFile(file);
+  res.json({
+    caption: caption,
+    image: result.url,
+  });
 }
 
 module.exports = {
