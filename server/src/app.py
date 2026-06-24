935422d41f4a54f66e81b144f3ac534e0152763b
Danesh <techydanesh@gmail.com>
2026-06-25 02:08:44 +0530

    frontend communication with cors

--- server/src/app.py
+++ server/src/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from src.routes.captionRoute import router as caption_router
 from src.core.handlers import register_exception_handlers
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 register_exception_handlers(app)
 
