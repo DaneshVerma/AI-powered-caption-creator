84b0799d0d688ea1218c3291abdd8b4e66d00718
Danesh <techydanesh@gmail.com>
2026-06-27 14:37:17 +0530

    logging

--- server/src/db/db.py
+++ server/src/db/db.py
@@ -2,14 +2,15 @@ from beanie import init_beanie
 from src.core.config import settings
 from src.db.models.user import User
 from pymongo import AsyncMongoClient
-
+from src.core.logger import logger
 
 client = AsyncMongoClient(settings.MONGO_URI)
 db = client[settings.DB_NAME]
+
 
 async def init_db():
     await init_beanie(
         database=db,
         document_models=[User],
     )
-    print("connected to db")
+    logger.info("MongoDB connected successfully")
